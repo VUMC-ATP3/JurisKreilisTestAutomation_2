7f53622584d013a36d28710c28439791e77c176f
iblinova <iblinova3@gmail.com>
2022-06-22 15:01:29 +0300

    initial commit

---
+++ src/test/java/FirstSeleniumTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+public class FirstSeleniumTest {
+    WebDriver chromeParluks;
+
+    @Test
+    public void testPageTitle(){
+        this.chromeParluks = new ChromeDriver();//atvert parlukprogrammu
+        this.chromeParluks.navigate().to("https://www.lu.lv/");
+        this.chromeParluks.manage().window().maximize();
+        String expectedTitle = "Latvijas Universitāte123";
+        String actualTitle = chromeParluks.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        chromeParluks.quit();
+    }
+
+}
